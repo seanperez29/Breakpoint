3ea20ff84b4681ee7157cf5fa649feb622b46ba1
Sean Perez <sean.perez29@gmail.com>
2017-09-06 13:39:48 -0600

    feat: add CreateGroupVC and connect outlets/actions

---
+++ Breakpoint/Controller/CreateGroupsVC.swift
@@ -0,0 +1,33 @@
+//
+//  CreateGroupsVC.swift
+//  Breakpoint
+//
+//  Created by Sean Perez on 9/6/17.
+//  Copyright © 2017 SeanPerez. All rights reserved.
+//
+
+import UIKit
+
+class CreateGroupsVC: UIViewController {
+
+    @IBOutlet weak var titleTextField: InsetTextField!
+    @IBOutlet weak var descriptionTextField: InsetTextField!
+    @IBOutlet weak var emailSearchTextField: InsetTextField!
+    @IBOutlet weak var emailSearchLabel: UILabel!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        
+    }
+    
+
+}
